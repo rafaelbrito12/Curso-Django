07275317d9144eccdc146052e65eb3bc883285c5
rafael <rafaelbrito.rb@outlook.com>
2022-09-05 23:13:36 -0300

    configurando templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
